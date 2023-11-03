9bff89c2d6824798f9afdd88c63c96e91f1e92a4
aaronchn99 <36250963+aaronchn99@users.noreply.github.com>
2023-11-03 03:39:36 +0000

    Feat: AI player - Random player as placeholder

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,11 +2,13 @@
 
 void displayWelcome();
 void displayBoard(uint8_t* board);
+uint8_t playHuman(uint8_t* board, char playerKey);
+uint8_t playCPU(uint8_t* board);
 
 uint8_t* board;
 
 int main() {
-    int x,y;
+    srand(time(nullptr));
     uint8_t currentPlayer = 1;
     bool isWin = false;
     board = new uint8_t[9]();
@@ -14,16 +16,9 @@ int main() {
     prompt("==============================Type Any Key To Start=============================");
     displayBoard(board);
     for (int t=0; t<9; t++) {
-        do {
-            coord input = promptCoord(
-                (string) "Player "+PLAYER_KEY[currentPlayer]+", pick your column [1-3] ",
-                (string) "Player "+PLAYER_KEY[currentPlayer]+", pick your row [1-3] "
-            );
-            x = input.x; y = input.y;
-            if (board[y*3+x] > 0)
-                cout << "That cell has been filled in, select another one" << endl;
-        } while (board[y*3+x] > 0);
-        board[3*y+x] = currentPlayer;
+        uint8_t cell;
+        cell = (currentPlayer == 1) ? playHuman(board, PLAYER_KEY[currentPlayer]) : playCPU(board);
+        board[cell] = currentPlayer;
         displayBoard(board);
         if (isWin = checkWin(currentPlayer, board)) break;
         (currentPlayer == 1) ? currentPlayer++ : currentPlayer--;
@@ -31,6 +26,28 @@ int main() {
     if (isWin) prompt((string) "Yaass! Player "+PLAYER_KEY[currentPlayer]+" has won! Type any key to exit ");
     else prompt((string) "Eh! It's a draw... Type any key to exit ");
     return 0;
+}
+
+uint8_t playHuman(uint8_t* board, char playerKey) {
+    uint8_t cell;
+    do {
+        coord input = promptCoord(
+            (string) "Player "+playerKey+", pick your column [1-3] ",
+            (string) "Player "+playerKey+", pick your row [1-3] "
+        );
+        cell = input.y*3 + input.x;
+        if (board[cell] > 0)
+            cout << "That cell has been filled in, select another one" << endl;
+    } while (board[cell] > 0);
+    return cell;
+}
+
+uint8_t playCPU(uint8_t* board) {
+    int cell;
+    do {
+        cell = 9*((float) rand()/RAND_MAX);
+    } while (board[cell] > 0); 
+    return cell;
 }
 
 void displayWelcome() {
